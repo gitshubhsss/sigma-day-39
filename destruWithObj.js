eba769363097f299b8653d56745ba836904419e9
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-23 21:21:55 +0530

    destructing using obj

---
+++ destruWithObj.js
@@ -0,0 +1,11 @@
+//destructuring with objects
+let student={
+    name:"shubham",
+    marks:84,
+    roll:4044,
+    class:"MSc computer science"
+}
+
+let {name:sname,marks:smarks}=student;
+
+console.log(sname);
